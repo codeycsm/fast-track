7b00ce79d1a13aabe0b91c0f38a469a43c10c51e
Codey <codeymcdaniel@gmail.com>
2019-02-22 09:53:00 -0700

    Created orm for users past fasts to store in separate table

--- models/userDataOrm.js
+++ models/userDataOrm.js
@@ -1,1 +1,12 @@
-module.exports = function() {};
+module.exports = function(sequelize, DataTypes) {
+  let pastFast = sequelize.define("pastFast", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    startTime: DataTypes.DATE,
+    endTime: DataTypes.DATE,
+    totalTime: DataTypes.DECIMAL
+  });
+};
