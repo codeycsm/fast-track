15ee015cf86c333701c3447b2c98b24724e5d9e7
Codey <codeymcdaniel@gmail.com>
2019-02-23 11:54:49 -0700

    removed passwordfield

--- models/userOrm.js
+++ models/userOrm.js
@@ -1,7 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
   let User = sequelize.define("User", {
     username: DataTypes.STRING,
-    password: DataTypes.STRING,
     startTime: DataTypes.DATE
   });
   User.associate = function(models) {
