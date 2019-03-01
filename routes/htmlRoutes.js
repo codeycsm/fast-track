9a606369f50af465c0aa587ce03cf5a88a728372
Jamison Gordon <jgordon@uhin.org>
2019-02-28 20:34:44 -0700

    Added initial timer page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,14 @@
 // HTML Routes
 let express = require("express");
-console.log("HTML routes connected");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+      res.render("index");
+});
+
+router.get('/timer', function(req, res) {
+    res.render("timer");
+});
+
+module.exports = router;
