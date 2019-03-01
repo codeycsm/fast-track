f2773430631b2533cee7b82a916698abe60e8779
Jamison Gordon <jgordon@uhin.org>
2019-02-28 20:39:36 -0700

    Fixed stupid stupid indentation

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 let db = require("../models");
 
 router.get("/", function(req, res) {
-      res.render("index");
+    res.render("index");
 });
 
 router.get('/timer', function(req, res) {
