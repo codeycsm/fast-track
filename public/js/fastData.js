4e99d1f7341427c95fa8032c2fb28a414b801f2f
Codey <codeymcdaniel@gmail.com>
2019-03-05 19:03:18 -0700

    added signout button to data page

--- public/js/fastData.js
+++ public/js/fastData.js
@@ -1,15 +1,16 @@
-$("#fastData").hide();
-let foundUser = findUser();
-if (foundUser) {
-  let user = JSON.parse(localStorage.getItem("fastTrack"));
-  $("#username").html(user.username + "'s ");
-  $.ajax({
-    url: "/past-data",
-    type: "POST",
-    data: { user }
-  }).then(function(result) {
-    for (let i = 0; i < result.length; i++) {
-      $("#fastData").append(`
+$(document).ready(function() {
+  $("#fastData").hide();
+  let foundUser = findUser();
+  if (foundUser) {
+    let user = JSON.parse(localStorage.getItem("fastTrack"));
+    $("#username").html(user.username + "'s ");
+    $.ajax({
+      url: "/past-data",
+      type: "POST",
+      data: { user }
+    }).then(function(result) {
+      for (let i = 0; i < result.length; i++) {
+        $("#fastData").append(`
         <div class="row pastData">
           <div class="col s4 center-align">
             ${moment(result[i].startTime).format("M/D/Y h:mm:ss a")}
@@ -18,22 +19,20 @@ if (foundUser) {
             ${moment(result[i].endTime).format("M/D/Y h:mm:ss a")}
           </div>
           <div class="col s4 center-align">
-<<<<<<< HEAD
-            ${moment(result[i].endTime).diff(
-              moment(result[i].startTime),
-              "hours"
-            )} hour(s)
-=======
-            ${formatTime(moment(result[i].endTime).diff(
-              moment(result[i].startTime))
+            ${formatTime(
+              moment(result[i].endTime).diff(moment(result[i].startTime))
             )}
->>>>>>> master
           </div>
         </div>
       `);
-    }
+      }
+    });
+    $("#fastData").show();
+  } else {
+    window.location = "/";
+  }
+
+  $("#logout").on("click", function() {
+    signOut();
   });
-  $("#fastData").show();
-} else {
-  window.location = "/";
-}
+});
